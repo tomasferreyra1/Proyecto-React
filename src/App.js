93535519e618f7f840cdf3c8376be09f9eafe1c5
Tomas Ferreyra <tomy2002.tf@gmail.com>
2022-09-26 22:11:59 -0300

    Desafio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ItemDetailContainer from './components/Products/ItemDetailContainer/ItemD
 import AboutUs from './components/AboutUs/AboutUs';
 import Contact from './components/Contact/Contact';
 import CartProvider  from './Context/CartContext';
-import CartContainer from './components/Products/CartContainer/cartContainer'
+import CartContainer from './components/Products/CartContainer/CartContainer'
 import {BrowserRouter,Route,Routes} from 'react-router-dom' 
 
 
